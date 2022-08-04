c9d89e84bd726204052e8318e32ea3836ecb3c81
Luis Correa <luiscorrea874@gmail.com>
2022-08-04 08:11:11 -0500

    Hola munod, constantes y variables

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let mensaje
+
+mensaje = "Hola mundo";
+//alert(mensaje);
+
+mensaje = "Hola mundo dos";
+//alert(mensaje);
+
+let a = 3, b = 4, c = 5;
+console.log(a+b)
+
+let nombre = "Luis";
+let apellido = "Correa";
+
+console.log("Hola",nombre,apellido);
+
+const PI = 3.14;
