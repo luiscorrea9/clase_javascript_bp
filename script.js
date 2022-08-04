8988934890bbbc694d473a260aae90f0c7d2f191
Luis Correa <luiscorrea874@gmail.com>
2022-08-04 11:22:42 -0500

    Ejercicios

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ let apellido = "Correa";
 console.log("Hola",nombre,apellido);
 
 const PI = 3.14;
+  
+
+//Infinity: representa el infinito matematico. Valor que es MAYOR que cualquier cosa
+console.log(1/0);
+
+//NaN: representa un error de cálculo
+console.log("Hola"/5);
+
+//BigInt: valores mayores que (2^53-1) o menor que -(2^53-1)
+
+//String
+
+let texto = "hola";
+
+//funciones
+
+function sumar(a , b){
+    return a+b;
+}
+
+function descuento(valor){
+    let valorDescuento=valor*0.2;
+    let valorFinal = valor - valorDescuento;
+    
+    return`El valor del producto inicial es ${valor} 
+    \n El valor con descuento es ${valorFinal} 
+    \n El valor ahorrado es ${valorDescuento}`;
+
+}
+
+console.log(descuento(150000))
+
+
+
